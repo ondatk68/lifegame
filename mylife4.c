245d0e0b2e04c337a6fa8bf249a004608f472f86
恩田健太郎 <ondakentaro0805@g.ecc.u-tokyo.ac.jp>
2021-12-15 02:30:23 +0900

    複数種に対応

--- mylife4.c
+++ mylife4.c
@@ -5,19 +5,45 @@
 #include <math.h>
 #include <string.h>
 
-
+int sum(int n, int list[n]){
+    int s=0;
+    for(int i=0; i<n; i++){
+        s+=list[i];
+    }
+    return s;
+}
+int max(int n, int list[n]){
+    int m=0;
+    for(int i=0; i<n; i++){
+        if(list[i]>=m){
+            m=list[i];
+        }
+    }
+    return m;
+}
 /*
  ファイルによるセルの初期化: 生きているセルの座標が記述されたファイルをもとに2次元配列の状態を初期化する
  fp = NULL のときは、関数内で適宜定められた初期状態に初期化する。関数内初期値はdefault.lif と同じもの
  */
-int my_init_cells(const int height, const int width, int cell[height][width], FILE* fp){
+int my_init_cells(const int height, const int width, int cell[height][width], FILE* fp, int n_species){
     if(fp==NULL){
         
         int r[10]={1,0,0,0,0,0,0,0,0,0}; //alive:dead = 1:9
+        int species[n_species];
+        for(int i=0; i<n_species; i++){
+            species[i]=i+1;
+        }
         srand((unsigned int)time(NULL));
+        int tmp;
         for(int y=0; y<height; y++){
             for(int x=0; x<width; x++){
-                cell[y][x]=r[rand()%10];
+                tmp = r[rand()%10];
+                if(tmp){
+                    cell[y][x]=species[rand()%n_species];
+                }else{
+                    cell[y][x]=0;
+                }
+                
             }
         }
     }else{
@@ -124,19 +150,44 @@ int my_init_cells(const int height, const int width, int cell[height][width], FI
 /*
  グリッドの描画: 世代情報とグリッドの配列等を受け取り、ファイルポインタに該当する出力にグリッドを描画する
  */
-void my_print_cells(FILE *fp, int gen, const int height, const int width, int cell[height][width], char rule[]){
+void my_print_cells(FILE *fp, int gen, const int height, const int width, int cell[height][width], char rule[], int n_species){
     //生きている個数を数え上げ
-    int alive=0;
+    int alive[n_species];
+    char color[4]={'R', 'G', 'Y', 'B'};
+    
+    for(int i=0; i<n_species; i++){
+        alive[i]=0;
+    }
     for(int y=0; y<height; y++){
         for(int x=0; x<width; x++){
             if(cell[y][x]){
-                alive++;
+                alive[cell[y][x]-1]++;
             }
         }
     }
+    int alive_ratio[n_species];
+    for(int i=0; i<n_species; i++){
+        alive_ratio[i]=round((double)alive[i]/(height*width)*(width+2));
+    }
 
 
-    fprintf(fp,"generation = %5d, alive: %4d, dead: %4d, rule: %s\n", gen, alive, height*width-alive, rule); // この場合 (fp = stdout), printfと同じ
+    fprintf(fp,"rule: %s, gen = %5d, ", rule, gen /*alive, height*width-sum(n_species, alive), rule*/); // この場合 (fp = stdout), printfと同じ
+    for(int i=0; i<n_species; i++){
+        fprintf(fp,"%c: %4d, ", color[i], alive[i]);
+    }
+    fprintf(fp,"dead: %4d\n", height*width-sum(n_species, alive));
+
+    for(int i=0; i<n_species; i++){
+        for(int j=0; j<alive_ratio[i]; j++){
+            fprintf(fp, "\e[%dm#\e[0m", 31+i);
+        }
+    }
+    for(int i=0; i<width+2-sum(n_species,alive_ratio); i++){
+        fprintf(fp,"#");
+    }
+    fprintf(fp,"\n");
+
+
     fprintf(fp,"+");
     for (int x = 0 ; x < width ; x++)
         fprintf(fp, "-");
@@ -149,8 +200,16 @@ void my_print_cells(FILE *fp, int gen, const int height, const int width, int ce
         // ANSIエスケープコードを用いて、赤い"#" を表示
         // \e[31m で 赤色に変更
         // \e[0m でリセット（リセットしないと以降も赤くなる）
-            if(cell[y][x]){                
-                fprintf(fp, "\e[31m#\e[0m");               
+            if(cell[y][x]){  
+                if(cell[y][x]==1){              
+                    fprintf(fp, "\e[31m#\e[0m"); 
+                }else if(cell[y][x]==2){
+                    fprintf(fp, "\e[32m#\e[0m"); 
+                }else if(cell[y][x]==3){
+                    fprintf(fp, "\e[33m#\e[0m"); 
+                }else if(cell[y][x]==4){
+                    fprintf(fp, "\e[34m#\e[0m"); 
+                }             
             }
             else{
                 fprintf(fp, " ");
@@ -171,26 +230,29 @@ void my_print_cells(FILE *fp, int gen, const int height, const int width, int ce
 /*
  着目するセルの周辺の生きたセルをカウントする関数
  */
-int my_count_adjacent_cells(int h, int w, const int height, const int width, int cell[height][width]){
+void my_count_adjacent_cells(int h, int w, const int height, const int width, int cell[height][width], int n_species, int n_count[n_species]){
     int d[8][2] = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
-    int count=0;
+    for(int i=0; i<n_species; i++){
+        n_count[i]=0;
+    }
+    //int count=0;
     for(int i=0; i<8; i++){
         if(0<=h+d[i][0] && h+d[i][0]<height && 0<=w+d[i][1] && w+d[i][1]<width){
             if(cell[h+d[i][0]][w+d[i][1]]){
-                count++;
+                n_count[cell[h+d[i][0]][w+d[i][1]]-1]++;
             }
         }
     }
 
-    return count;
+    //return count;
 }
 
 void make_rule(char rule[], int born[], int survive[]){
-    char _born[10];
-    char _survive[10];
+    char _born[15];
+    char _survive[15];
 
-    strcpy(_born,strtok(rule,"/\n"));
-    strcpy(_survive,strtok(NULL,"/\n"));
+    strcpy(_born,strtok(rule,"/"));
+    strcpy(_survive,strtok(NULL,"/"));
 
     for(int i=1; i<strlen(_born); i++){
         born[i-1]=_born[i]-'0';
@@ -200,29 +262,32 @@ void make_rule(char rule[], int born[], int survive[]){
     }
 }
 
+
+
 /*
  ライフゲームのルールに基づいて2次元配列の状態を更新する
  */
-void my_update_cells(const int height, const int width, int cell[height][width], int born[10], int survive[10]){
+void my_update_cells(const int height, const int width, int cell[height][width], int born[10], int survive[10], int n_species){
     int newcell[height][width];
+    int n_around[n_species];
 
     for(int y=0; y<height; y++){
         for(int x=0; x<width; x++){
-            int around=my_count_adjacent_cells(y,x,height,width,cell);
+            my_count_adjacent_cells(y,x,height,width,cell,n_species,n_around);
             int bcheck=0;
             int scheck=0;
             
             if(cell[y][x]){
                 for(int i=0; i<10; i++){
                     if(survive[i] != -1){
-                        if(around==survive[i]){
+                        if(sum(n_species,n_around)==survive[i]){
                             scheck=1;
                             break;
                         }
                     }
                 }
                 if(scheck){
-                    newcell[y][x]=1;
+                    newcell[y][x]=cell[y][x];
                 }else{
                     newcell[y][x]=0;
                 }
@@ -230,14 +295,19 @@ void my_update_cells(const int height, const int width, int cell[height][width],
             }else{
                 for(int i=0; i<10; i++){
                     if(born[i] != -1){
-                        if(around==born[i]){
+                        if(sum(n_species,n_around)==born[i]){
                             bcheck=1;
                             break;
                         }
                     }
                 }
                 if(bcheck){
-                    newcell[y][x]=1;
+                    int m=max(n_species,n_around);
+                    int new_i=rand()%n_species;
+                    while(n_around[new_i]!=m){
+                        new_i=rand()%n_species;
+                    }
+                    newcell[y][x]=new_i+1;
                 }else{
                     newcell[y][x]=0;
                 }
@@ -261,7 +331,7 @@ int main(int argc, char **argv)
   const int height = 40;
   const int width = 70;
   char Rule[30];
-
+  int n_species=1;
   int cell[height][width];
   for(int y = 0 ; y < height ; y++){
     for(int x = 0 ; x < width ; x++){
@@ -278,22 +348,32 @@ int main(int argc, char **argv)
     strcpy(Rule,argv[1]);
     FILE *lgfile;
     if ( (lgfile = fopen(argv[2],"r")) != NULL ) {
-      if(my_init_cells(height,width,cell,lgfile)==-1){//ファイルで初期化してみて盤面サイズがおかしかったらエラー
+      if(my_init_cells(height,width,cell,lgfile,1)==-1){//ファイルで初期化してみて盤面サイズがおかしかったらエラー
           return EXIT_FAILURE;
       }; // ファイルによる初期化
     }
     else{
-      fprintf(stderr,"cannot open file %s\n",argv[2]);
-      return EXIT_FAILURE;
+        if(atoi(argv[2])){
+            n_species=atoi(argv[2]);
+            if(1<=n_species && n_species<=4){
+                my_init_cells(height,width,cell,NULL,n_species);
+            }else{
+                fprintf(stderr,"too large number of species: %d\n",n_species);
+                return EXIT_FAILURE;
+            }
+        }else{
+            fprintf(stderr,"cannot open file %s\n",argv[2]);
+            return EXIT_FAILURE;
+        }
     }
     fclose(lgfile);
   }
-  else if(argc==2){//1個の場合は、ルールの指定のみで初期盤面はランダム。
-      my_init_cells(height,width,cell,NULL);
+  else if(argc==2){//1個の場合は、ルールの指定のみで初期盤面はランダム。種数は1。
+      my_init_cells(height,width,cell,NULL,1);
       strcpy(Rule,argv[1]);
   }
-  else{//引数なしは、初期盤面ランダムでルールはConway's life
-    my_init_cells(height, width, cell, NULL); // デフォルトの初期値を使う
+  else{//引数なしは、初期盤面ランダムでルールはConway's life。種数は1。
+    my_init_cells(height, width, cell, NULL,1); // デフォルトの初期値を使う
     strcpy(Rule,"B3/S23");
   }
     
@@ -309,15 +389,24 @@ int main(int argc, char **argv)
 
     make_rule(c_rule, born,survive);
 
+    /*
+    for(int i=0; i<10; i++){
+        printf("%d, ", born[i]);
+    }
+    printf("\n");
+    for(int i=0; i<10; i++){
+        printf("%d, ", survive[i]);
+    }
+    printf("\n");*/
 
-  my_print_cells(fp, 0, height, width, cell, Rule); // 表示する
+  my_print_cells(fp, 0, height, width, cell, Rule, n_species); // 表示する
   
   char filename[21]="./output/gen__00.lif"; //outputフォルダに盤面ファイルを吐き出す
   /* 世代を進める*/
   int gen=1;
   while(1){
-    my_update_cells(height, width, cell, born, survive); // セルを更新
-    my_print_cells(fp, gen, height, width, cell, Rule);  // 表示する
+    my_update_cells(height, width, cell, born, survive, n_species); // セルを更新
+    my_print_cells(fp, gen, height, width, cell, Rule, n_species);  // 表示する
     usleep(0.1*1000*1000); //0.1秒休止する
     if(gen%100==0 && gen<10000){
         filename[12]=gen/1000+'0';
@@ -334,7 +423,7 @@ int main(int argc, char **argv)
         }
         fclose(output);
     }
-    fprintf(fp,"\e[%dA",height+3);//height+3 の分、カーソルを上に戻す(壁2、表示部1)
+    fprintf(fp,"\e[%dA",height+4);//height+3 の分、カーソルを上に戻す(壁2、表示部1)
     gen++;
   }
 
